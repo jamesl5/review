8b2e6e431a7dbf4ea368501d3b16dc7f4bea7e73
jamesl5@uw.edu <jamesl5@uw.edu>
2015-10-27 10:15:34 +0900

    Third

--- js/review.js
+++ js/review.js
@@ -2,17 +2,23 @@
 Parse.initialize('5ZUKzcsOobn0VZza2IcvHdiAAv2LxHajSWqFmBX6', 'rqwnj98pPdSTOeKPIGKAy3Oawd5JCmEKpyQLEiat');
 
 var Review = Parse.Object.extend('Review');
-$('#average').raty({ score: 3 });
+$(document).ready(function(){
+	
 
+$('#rating').raty({ 
+	path: 'images',
+	score: 3
+	
+});
 // Click event when form is submitted
 $('form').submit(function() {
 	
-	var review = new Review();
-	var rating = parseInt($('#review_rating').val(), 10);
+	var review = new Review();	
+	var rating = $('#rating').raty('score');
+	var body = $('#review_body').val();
 	review.set('review_title', $('#review_title').val());
-	review.set('review_body', $('#review_body').val());
+	review.set('review_body', body);
 	review.set('review_rating', rating);
-	//review.set("review_rating", number);
 	review.save(null, {
 		success:getData
 	})
@@ -24,7 +30,6 @@ $('form').submit(function() {
 // Write a function to get data
 var getData = function() {
 	
-
 	// Set up a new query for our Music class
 	var query = new Parse.Query(Review)
 
@@ -46,28 +51,49 @@ var getData = function() {
 var buildList = function(data) {
 	// Empty out your ordered list
 	$('ul').empty()
-	var total;
+	var total = 0;
 	// Loop through your data, and pass each element to the addItem function
-	data.forEach(function(d, total){
-		addItem(d, total);
+	data.forEach(function(d){
+		total += addItem(d);
 	})
+	var avg = total/data.length;
+	$('#average').raty({ 
+		path: 'images',
+		readOnly : true,
+		score : avg
+	});
 }
 var total = function(item){
 	var rating = item.get('review_rating')
 }
 
 // This function takes in an item, adds it to the screen
-var addItem = function(item, total) {
+var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
 	var title = item.get('review_title');
 	var body = item.get('review_body');
 	var rating = item.get('review_rating');
-	total += rating;
+	
 	// Append li that includes text from the data item
-	var li = $('<li>' + title + '</li> ');
+	var form = $("<form></form>");
+	$('#reviews').append(form);
+	var row = $("<div class='row'><div id='rated'></div></div>");
+	$('form').append(row);
+	$('div:last').raty({ 
+		path: 'images',
+		readOnly : true,
+		score : rating
+	});
+	row = $("<div class='row'><div id='title'></div></div>");
+	$('form').append(row);
+	$('div:last').text(title);
+	row = $("<div class='row'><div id='body'></div></div>");
+	$('form').append(row);
+	$('div:last').text(body);
+	//var li = $('<li><h1>' + title + '</h1><br>' + body + '</li>' );
 	
 	// Create a button with a <span> element (using bootstrap class to show the X)
-	var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>');
+	/*var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>');
 	
 	// Click function on the button to destroy the item, then re-call getData
 	button.click(function() {
@@ -77,10 +103,11 @@ var addItem = function(item, total) {
 	})
 	
 	// Append the button to the li, then the li to the olF
-	li.append(button);
-	$('ul').append(li)
-	
+	li.append(button);*/
+	//
+	return rating;
 }
 
 // Call your getData function when the page loads
 getData()
+});
